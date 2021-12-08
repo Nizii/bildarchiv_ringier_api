dbeacd47495574774d5a19789ec18126bb3cd234
Nizam <nizam.oezdemir@gmx.ch>
2021-12-08 15:20:14 +0100

    URL in PhotoModel Klasse angepasst

--- WebApplication1/Models/PhotoModel.cs
+++ WebApplication1/Models/PhotoModel.cs
@@ -5,7 +5,7 @@ namespace WebApplication1.Models
     public class PhotoModel
     {
         //private const string PHOTO_URL = "http://localhost:5000/Photos/";
-        private const string PHOTO_URL = "C:/home/site/wwwroot/api/img/";
+        private const string PHOTO_URL = "https://bildarchivaarau.azurewebsites.net/photos/";
 
         public ObjectId Id { get; set; }
         public int PhotoId { get; set; }
