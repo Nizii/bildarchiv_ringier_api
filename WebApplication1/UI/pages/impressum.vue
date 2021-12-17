64d897b4e185b8327787a4e009195fa70278244b
Nizam <nizam.oezdemir@gmx.ch>
2021-12-17 14:53:10 +0100

    UI Ordner gelöscht UI neu auf Github von Patricia https://github.com/patriciqa/Stuwex-frontend.git

--- WebApplication1/UI/pages/impressum.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <main>
-        <navigation></navigation>
-        <p>Das Copyright der Bilder liegt beim Ringier Bildarchiv, Staatsarchiv Aargau. 
-    </p>
-    </main>
-</template>
